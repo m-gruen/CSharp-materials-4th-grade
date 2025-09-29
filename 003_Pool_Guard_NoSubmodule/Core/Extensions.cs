bcb7cc388074baba8f093cb54ae9ae171673a86e
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-09-29 17:33:18 +0200

    refactor: removed old project without submodule

--- 003_Pool_Guard_NoSubmodule/Core/Extensions.cs
+++
@@ -1,9 +0,0 @@
-namespace PoolGuard.Core;
-
-public static class Extensions
-{
-    public static ZonedDateTime ToLocalDateTime(this Instant self)
-    {
-        return self.InZone(Const.TimeZone);
-    }
-}
