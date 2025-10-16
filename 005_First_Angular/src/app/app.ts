151e4a1a7febd500f8288a6b62761745e9a8e71d
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-10-16 16:15:51 +0200

    feat: refactor app structure by adding greeting component and updating app layout

--- 005_First_Angular/src/app/app.ts
+++ 005_First_Angular/src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { Greeting } from './greeting/greeting';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatButton, Greeting],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('FirstAngular');
+  protected readonly clickCount: WritableSignal<number> = signal(0);
+
+  protected handleClick(): void {
+    this.clickCount.update((n: number) => n + 1);
+  }
 }
---
+++ 005_First_Angular/src/app/greeting/greeting.ts
@@ -0,0 +1,23 @@
+import { Component, computed, Signal, signal, WritableSignal } from '@angular/core';
+
+@Component({
+  selector: 'app-greeting',
+  imports: [],
+  templateUrl: './greeting.html',
+  styleUrl: './greeting.scss'
+})
+export class Greeting {
+  private readonly names: string[] = ['Alice', 'Bob Brown Bob Brown', 'Charlie', 'Diana'];
+  private currentNameIdx: WritableSignal<number> = signal(0);
+  protected readonly currentName: Signal<string> = computed(() => {
+    return this.names[this.currentNameIdx()];
+  });
+
+  protected greet(): void {
+    this.currentNameIdx.update(n => n + 1);
+
+    if (this.currentNameIdx() >= this.names.length) {
+      this.currentNameIdx.set(0);
+    }
+  }
+}
