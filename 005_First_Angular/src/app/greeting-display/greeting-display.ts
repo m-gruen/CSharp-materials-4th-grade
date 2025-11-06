b79401838fdc70a1ef673e3daedbc4251b62517b
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-11-06 15:29:39 +0100

    feat: add greeting display component with template and styles

---
+++ 005_First_Angular/src/app/greeting-display/greeting-display.ts
@@ -0,0 +1,28 @@
+import {Component, input, InputSignal, output, OutputEmitterRef} from '@angular/core';
+import {LocalDateTime} from '@js-joda/core';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
+
+@Component({
+  selector: 'app-greeting-display',
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent
+  ],
+  templateUrl: './greeting-display.html',
+  styleUrl: './greeting-display.scss'
+})
+export class GreetingDisplay {
+  public readonly name: InputSignal<string> = input.required<string>();
+  public readonly onClicked: OutputEmitterRef<ClickEvent> = output()
+  protected readonly timestamp: LocalDateTime = LocalDateTime.now();
+
+  protected handleClick(type: TitleOrContent): void {
+    const clickEvent: ClickEvent = [type, this.name()];
+    this.onClick.emit(clickEvent);
+  }
+}
+
+export type ClickEvent = [TitleOrContent, string];
+export type TitleOrContent = "title" | "content";
