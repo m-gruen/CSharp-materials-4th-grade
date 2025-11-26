6096ccdca0b5dda49d998e9ff8ed733794cccb42
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-11-26 08:43:33 +0100

    feat: enhance contact component with form model and minimum message length

--- 005_First_Angular/frontend/src/app/contact/contact.ts
+++ 005_First_Angular/frontend/src/app/contact/contact.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, signal, WritableSignal} from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './contact.scss'
 })
 export class Contact {
+  protected readonly MESSAGE_MIN_LENGTH : number = 10;
+  private  readonly  contactFormModel: WritableSignal<ContactFormModel> = signal({
+    name: '',
+    email: '',
+    message: ''
+  });
 
 }
+
+type ContactFormModel = {
+  name: string;
+  email: string;
+  message: string;
+};
