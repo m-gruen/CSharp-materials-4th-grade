4240fcfb1523dceb694ec3c9a87e9136359b1b1c
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-11-27 13:50:38 +0100

    feat: implement form validation for contact component

--- 005_First_Angular/frontend/src/app/contact/contact.ts
+++ 005_First_Angular/frontend/src/app/contact/contact.ts
@@ -1,4 +1,5 @@
 import {Component, signal, WritableSignal} from '@angular/core';
+import {debounce, email, form, minLength, required} from '@angular/forms/signals';
 
 @Component({
   selector: 'app-contact',
@@ -7,13 +8,25 @@ import {Component, signal, WritableSignal} from '@angular/core';
   styleUrl: './contact.scss'
 })
 export class Contact {
-  protected readonly MESSAGE_MIN_LENGTH : number = 10;
-  private  readonly  contactFormModel: WritableSignal<ContactFormModel> = signal({
+  protected readonly MESSAGE_MIN_LENGTH: number = 10;
+  private readonly contactFormModel: WritableSignal<ContactFormModel> = signal({
     name: '',
     email: '',
     message: ''
   });
 
+  protected readonly contactForm = form(this.contactFormModel, path => {
+    required(path.name, {message: "Name is required."});
+    required(path.email, {message: "Email is required."});
+    required(path.message, {message: "Message is required."});
+    email(path.email, {message: "Has to be a valid email address."});
+    debounce(path.email, 250);
+    minLength(
+      path.message,
+      this.MESSAGE_MIN_LENGTH,
+      {message: `Message must be at least ${this.MESSAGE_MIN_LENGTH} characters long.`}
+    );
+  })
 }
 
 type ContactFormModel = {
