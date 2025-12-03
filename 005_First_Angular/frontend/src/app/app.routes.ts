3201f01b836441e797963de215dfa729fd5bd713
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-12-03 08:53:13 +0100

    feat: implement lazy loading for exhibit edit routes and enhance news display with deferred rendering

--- 005_First_Angular/frontend/src/app/app.routes.ts
+++ 005_First_Angular/frontend/src/app/app.routes.ts
@@ -6,12 +6,16 @@ import {ExhibitDetail} from './exhibit-details/exhibit-details';
 import {ExhibitEdit} from './exhibit-edit/exhibit-edit';
 import {authGuard} from '../core/util/auth-guard';
 
+const exhibitEditImport =
+  () => import('./exhibit-edit/exhibit-edit')
+    .then(m => m.ExhibitEdit);
+
 export const routes: Routes = [
   {path: 'news', component: News},
   {path: 'contact', component: Contact},
   {path: 'exhibits', component: ExhibitList},
   {path: 'exhibits/:id', component: ExhibitDetail},
-  {path: 'exhibit-edit', component: ExhibitEdit, canActivate: [authGuard]},
-  {path: 'exhibit-edit/:id', component: ExhibitEdit, canActivate: [authGuard]},
+  {path: 'exhibit-edit', loadComponent: exhibitEditImport, canActivate: [authGuard]},
+  {path: 'exhibit-edit/:id', loadComponent: exhibitEditImport, canActivate: [authGuard]},
   {path: '', redirectTo: 'news', pathMatch: 'full'}
 ];
